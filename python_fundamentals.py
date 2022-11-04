ab7d1cd2e60a47a2ee82f4af6930ddf49c9b0c7c
root <root@HPEnvyx360-PhilK.localdomain>
2022-11-04 10:22:34 -0700

    create functional lis_sayer funciton

--- python_fundamentals.py
+++ python_fundamentals.py
@@ -1,2 +1,11 @@
-def list_sayer()
-  
+def list_sayer(test_list):
+  output_str = None
+  if test_list == []:
+    print ('The list is empty.')
+    return False
+  else 
+    for i in test_list:
+      output_str = test_list[i]
+      print(f'Your list item is: {output_str} and it is at index: {i}')
+    return True
+
