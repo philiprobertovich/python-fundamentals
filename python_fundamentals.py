9ef7c61d6c269bc165834c370a6be0f195e4332d
root <root@HPEnvyx360-PhilK.localdomain>
2022-11-04 10:57:17 -0700

    create working dict_sayer function

--- python_fundamentals.py
+++ python_fundamentals.py
@@ -1,11 +1,22 @@
 def list_sayer(test_list):
+  index = 0
   output_str = None
   if test_list == []:
     print ('The list is empty.')
     return False
-  else 
-    for i in test_list:
-      output_str = test_list[i]
-      print(f'Your list item is: {output_str} and it is at index: {i}')
+  else:
+    for item in test_list:
+      output_str = item
+      print(f'Your list item is: {output_str} and it is at index: {index}')
+      index += 1
+    return True
+
+def dict_sayer(test_dict):
+  if test_dict == {}:
+    print('Your dictionary is empty.')
+    return False
+  else:
+    for keys, values in test_dict.items():
+      print(f'Your key is: {keys}, and your value is: {values}.')
     return True
 
