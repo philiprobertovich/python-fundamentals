1f861aaa7dda57aa662a126a6ebe92751fc7d810
root <root@HPEnvyx360-PhilK.localdomain>
2022-11-04 14:56:45 -0700

    add the remaining functionality to the zipper function, as well as format all the rest of the working functions properly with tests and test variables.

--- python_fundamentals.py
+++ python_fundamentals.py
@@ -1,3 +1,13 @@
+test_list = ['apple', 'orange', 'baseball', 'tiger']
+test_dict = {
+  'a': 1,
+  'b': 2,
+  'c': 3
+  }
+list_1 = ["vanilla", "cherry"]
+list_2 = ["cake", "ice_cream"]
+list_1a = ["vanilla", "cherry", "pistachio"]
+
 def list_sayer(test_list):
   index = 0
   output_str = None
@@ -11,6 +21,9 @@ def list_sayer(test_list):
       index += 1
     return True
 
+print(list_sayer(test_list))
+print(list_sayer([]))
+
 def dict_sayer(test_dict):
   if test_dict == {}:
     print('Your dictionary is empty.')
@@ -20,15 +33,27 @@ def dict_sayer(test_dict):
       print(f'Your key is: {keys}, and your value is: {values}.')
     return True
 
+print(dict_sayer(test_dict))
+print(dict_sayer({}))
+
 def greatest(test_dict):
   for keys, values in test_dict.items():
-    if value == max(test_dict.values()):
+    if values == max(test_dict.values()):
       return (keys, values)
+
+print(greatest(test_dict))
 
 def zipper(list_1, list_2):
   dict_a = {}
-  list_a = []  
+  list_a = [] 
+  tuple_a = () 
   if len(list_1) == len(list_2):
     list_a = list(zip(list_1, list_2))
     dict_a.update(list_a)
-    print(dict_a)   
+    return dict_a   
+  else:
+    tuple_a = (list_1, len(list_1), list_2, len(list_2))
+    return tuple_a
+
+print(zipper(list_1, list_2))
+print(zipper(list_1a, list_2))
